ce19f16dbcda33dc3f8269367bc72d5708c524f4
Nishchal Gautam <nishchal@honestbank.com>
2022-10-28 16:15:09 +0700

    ci: remove remote integration test

--- v2/producer/producer_integration_test_remote_test.go
+++ v2/producer/producer_integration_test_remote_test.go
@@ -17,21 +17,9 @@ type BenchmarkMessage struct {
 }
 
 func TestNewCanConnectToLabEnvironment(t *testing.T) {
-	_ = os.Setenv("KP_USERNAME", "OBHGFSK7ZAVNEDFV")
-	_ = os.Setenv("KP_PASSWORD", "P5yMMU05T9MNiJ9FHUINoEpm7PeW0huuyuF0H01FEZe4YcDBkdkYRI0nAi/7/bFv")
-	_ = os.Setenv("KP_KAFKA_BOOTSTRAP_SERVERS", "pkc-ew3qg.asia-southeast2.gcp.confluent.cloud:9092")
 	_ = os.Setenv("KP_SASL_MECHANISM", "PLAIN")
 	_ = os.Setenv("KP_SECURITY_PROTOCOL", "SASL_SSL")
-	_ = os.Setenv("KP_SCHEMA_REGISTRY_ENDPOINT", "https://psrc-3w372.australia-southeast1.gcp.confluent.cloud")
-	_ = os.Setenv("KP_SCHEMA_REGISTRY_USERNAME", "SVZXV26UZ7TGPU7D")
-	_ = os.Setenv("KP_SCHEMA_REGISTRY_PASSWORD", "IbnoBsE83SbTqZjHznFcLxFsOtJoWzgw7CZunhVnOmtL8wJzhv9IVaVF0bVzLd9/")
 	defer func() {
-		_ = os.Unsetenv("KP_USERNAME")
-		_ = os.Unsetenv("KP_PASSWORD")
-		_ = os.Unsetenv("KP_KAFKA_BOOTSTRAP_SERVERS")
-		_ = os.Unsetenv("KP_SCHEMA_REGISTRY_ENDPOINT")
-		_ = os.Unsetenv("KP_SCHEMA_REGISTRY_USERNAME")
-		_ = os.Unsetenv("KP_SCHEMA_REGISTRY_PASSWORD")
 		_ = os.Unsetenv("KP_SASL_MECHANISM")
 		_ = os.Unsetenv("KP_SECURITY_PROTOCOL")
 	}()

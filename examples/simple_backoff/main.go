555a63a8d2aba13f20621a0f71f1448194b734e7
Nishchal Gautam <nishchal@honestbank.com>
2022-11-01 12:28:31 +0700

    feat: add middleware support

--- examples/simple_backoff/main.go
+++ examples/simple_backoff/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"strings"
 	"time"
@@ -16,9 +17,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// eventing.Setup(*cfg)
 	processor := kp.NewKafkaProcessor("test", "retry-test", "dead-test", 10, "simple-service", kp.KafkaConfig{KafkaBootstrapServers: strings.Split(cfg.KafkaConfig.KafkaBootstrapServers, ",")}, time.Second*2)
-	processor.Process(func(key string, message string, retries int, rawMessage *sarama.ConsumerMessage) error {
+	processor.Process(func(ctx context.Context, key string, message string, retries int, rawMessage *sarama.ConsumerMessage) error {
 		time.Sleep(time.Second * 60)
 		if message == "fail" {
 			return errors.New("failed")
@@ -26,6 +26,6 @@ func main() {
 		return nil
 	})
 
-	processor.Start()
+	processor.Start(context.Background())
 
 }
--- v2/interface.go
+++ v2/interface.go
@@ -1,7 +1,14 @@
 package v2
 
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+
+	"github.com/honestbank/kp/v2/internal/middleware"
+)
+
 type KafkaProcessor[MessageType any] interface {
 	WithRetry(retryTopic string, retryCount int) (KafkaProcessor[MessageType], error)
+	AddMiddleware(middleware middleware.Middleware[*kafka.Message, error]) KafkaProcessor[MessageType]
 	WithRetryOrPanic(retryTopic string, retryCount int) KafkaProcessor[MessageType]
 	WithDeadletter(deadLetterTopic string) (KafkaProcessor[MessageType], error)
 	WithDeadletterOrPanic(deadletterTopic string) KafkaProcessor[MessageType]

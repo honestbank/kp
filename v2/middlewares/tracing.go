4fa1386021e95287190c6261dcbfbbc6bac20c5c
Nishchal Gautam <nishchal@honestbank.com>
2022-11-09 16:17:42 +0700

    chore: set status

--- v2/middlewares/tracing.go
+++ v2/middlewares/tracing.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
 
 	"github.com/honestbank/kp/v2/internal/kafkaheaders"
 	"github.com/honestbank/kp/v2/internal/middleware"
@@ -19,6 +20,9 @@ func (t tracingMw) Process(ctx context.Context, item *kafka.Message, next func(c
 	err := next(ctx, item)
 	if err != nil {
 		span.RecordError(err)
+		// https://github.com/open-telemetry/opentelemetry-go/blob/2694dbfdba21d4f5496e873d7f69f04ada040af1/trace/trace.go#L356-L360
+		// we need to set status our selves
+		span.SetStatus(codes.Error, "error span")
 	}
 	span.End()
 	// we want to close before re-producing the message

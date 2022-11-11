5234a15955dbb94724706ea1c356050ce33a6fdf
Nishchal Gautam <nishchal@honestbank.com>
2022-11-11 14:17:24 +0700

    docs: update docs

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import Translate, {translate} from "@docusaurus/Translate";
+import Lottie from 'lottie-react'
+import code from '../../../static/lotties/code.json'
+import focus from '../../../static/lotties/focus.json'
+import tracing from '../../../static/lotties/tracing.json'
+import measurements from '../../../static/lotties/measurements.json'
+import easy from '../../../static/lotties/easy'
 
 type FeatureItem = {
     title: string;
-    Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+    lottie: any;
     description: JSX.Element;
 };
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, description, lottie}: FeatureItem): JSX.Element {
     return (
         <div className={clsx('col col--4')}>
             <div className="text--center">
-                <Svg className={styles.featureSvg} role="img"/>
+                <Lottie animationData={lottie} loop={true} style={{height: 300}} />
             </div>
             <div className="text--center padding-horiz--md">
                 <h3>{title}</h3>
                 <p>{description}</p>
             </div>
         </div>
-    );
+    )
 }
 
 export default function HomepageFeatures(): JSX.Element {
@@ -31,37 +37,31 @@ export default function HomepageFeatures(): JSX.Element {
                     {/*easy to use icon should be check check check icon (animating)*/}
                     <Feature
                         title={translate({message: "Easy to Use", id: "features.easy-to-use-title"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_mountain.svg').default}
+                        lottie={easy}
                         description={<Translate children={"With an intuitive API, you can configure the behavior of the library pretty easily. With no defaults, you have complete control over how your application behaves."} id={"features.easy-to-use-description"} description={"describe ease of use"} />}
                     />
                     {/*this can be insights icon, like graphs*/}
                     <Feature
                         title={translate({message: "Focus on What Matters", id: "features.stay_focused"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_tree.svg').default}
+                        lottie={focus}
                         description={<Translate description={"message explaining their business logic stays business logic, free of measurements, traces, retry logic etc."} id={"features.stay_focused_description"} children={"Leave non business logic to middlewares and focus on writing the business logic. Tracing, measurements, retries, etc are free from your business code and you can test just your business logic."}></Translate>}
-                    />
-                    {/*compilation returning error animation*/}
-                    <Feature
-                        title={translate({message: "Fully strongly typed", id: "features.strong_type.title"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_react.svg').default}
-                        description={<>{translate({message: "With the help of generics we provide fully typed messages. You don't need to worry about encoding/decoding a message. We work with binary formats so you don't have to.", id: "features.strong_type.description"})}</>}
                     />
                     {/*retry, retry, retry, fail! animation*/}
                     <Feature
-                        title={translate({message: "Easy retries, backoffs and deadlettering", id: "features.retries.title"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_react.svg').default}
-                        description={<>{translate({message: "To enable retries, backoffs and deadlettering, it's one line each!", id: "features.retries.description"})}</>}
+                        title={translate({message: "Measurements, retries and deadlettering", id: "features.retries.title"})}
+                        lottie={measurements}
+                        description={<>{translate({message: "Measurements, retries and deadlettering are one line each!", id: "features.retries.description"})}</>}
                     />
                     {/*traces icon?*/}
                     <Feature
-                        title={translate({message: "Automatic Tracing and measurements", id: "features.observability.title"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_react.svg').default}
-                        description={<>{translate({message: "Automatic tracing and measurements are one middleware each.", id: "features.observability.description"})}</>}
+                        title={translate({message: "Tracing and backoffs", id: "features.observability.title"})}
+                        lottie={tracing}
+                        description={<>{translate({message: "Tracing and backoffs are one middleware each.", id: "features.observability.description"})}</>}
                     />
                     {/*builder icon*/}
                     <Feature
                         title={translate({message: "Write your own middlewares", id: "features.middleware.title"})}
-                        Svg={require('@site/static/img/undraw_docusaurus_react.svg').default}
+                        lottie={code}
                         description={<>{translate({message: "Writing a middleware is very easy. Have a problem we didn't solve? Write your own middleware. Think it'd help the community? Send us a PR.", id: "features.observability.description"})}</>}
                     />
                 </div>

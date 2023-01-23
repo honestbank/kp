7864244833c1df887f98bb64b1ef27e26e43f6d1
Phathaisarn Chalermchuang <phathaisarn@honestbank.com>
2023-01-23 16:31:28 +0700

    fix: race condition

--- v2/kp.go
+++ v2/kp.go
@@ -2,6 +2,7 @@ package v2
 
 import (
 	"context"
+	"sync"
 
 	"github.com/honestbank/kp/v2/internal/middleware"
 )
@@ -11,8 +12,14 @@ type Processor[MessageType any] func(ctx context.Context, item *MessageType) err
 type kp[MessageType any] struct {
 	chain          middleware.Processor[*MessageType, error]
 	shouldContinue bool
+	mu             sync.Mutex
 }
 
+func (t *kp[MessageType]) getShouldContinue() bool {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return t.shouldContinue
+}
 func (t *kp[MessageType]) AddMiddleware(middleware middleware.Middleware[*MessageType, error]) MessageProcessor[MessageType] {
 	t.chain.AddMiddleware(middleware)
 
@@ -20,14 +27,16 @@ func (t *kp[MessageType]) AddMiddleware(middleware middleware.Middleware[*Messag
 }
 
 func (t *kp[MessageType]) Stop() {
+	t.mu.Lock()
 	t.shouldContinue = false
+	t.mu.Unlock()
 }
 
 func (t *kp[MessageType]) Run(processor Processor[MessageType]) error {
 	t.chain.AddMiddleware(middleware.FinalMiddleware[*MessageType, error](func(ctx context.Context, msg *MessageType) error {
 		return processor(ctx, msg)
 	}))
-	for t.shouldContinue {
+	for t.getShouldContinue() {
 		ctx := context.Background()
 		_ = t.chain.Process(ctx, nil)
 	}

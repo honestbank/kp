71786aae5852187d53df7e63d5f2e6cb57006f32
Nishchal Gautam <nishchal@honestbank.com>
2024-09-06 15:59:06 +0700

    feat: skip commit on failures

--- v2/kp.go
+++ v2/kp.go
@@ -12,6 +12,7 @@ type Processor[MessageType any] func(ctx context.Context, item *MessageType) err
 type kp[MessageType any] struct {
 	chain          middleware.Processor[*MessageType, error]
 	shouldContinue int32
+	onError        func(error)
 }
 
 func (t *kp[MessageType]) getShouldContinue() bool {
@@ -27,6 +28,13 @@ func (t *kp[MessageType]) AddMiddleware(middleware middleware.Middleware[*Messag
 func (t *kp[MessageType]) Stop() {
 	atomic.StoreInt32(&t.shouldContinue, 0)
 }
+func (t *kp[MessageType]) process() {
+	ctx := context.Background()
+	err := t.chain.Process(ctx, nil)
+	if err != nil {
+		t.onError(err)
+	}
+}
 
 func (t *kp[MessageType]) Run(processor Processor[MessageType]) error {
 	t.chain.AddMiddleware(middleware.FinalMiddleware[*MessageType, error](func(ctx context.Context, msg *MessageType) error {
@@ -34,16 +42,16 @@ func (t *kp[MessageType]) Run(processor Processor[MessageType]) error {
 	}))
 
 	for t.getShouldContinue() {
-		ctx := context.Background()
-		_ = t.chain.Process(ctx, nil)
+		t.process()
 	}
 
 	return nil
 }
 
-func New[MessageType any]() MessageProcessor[MessageType] {
+func New[MessageType any](onError func(err error)) MessageProcessor[MessageType] {
 	return &kp[MessageType]{
 		chain:          middleware.New[*MessageType, error](),
 		shouldContinue: 1,
+		onError:        onError,
 	}
 }
